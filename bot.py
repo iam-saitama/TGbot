8108638e7d0be36d7813c302e6899dbc1b40bfe8
iam-saitama <aidapdiamond@gmail.com>
2025-01-15 22:07:24 +0500

    s

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import telebot
 import buttons
 import database
+from geopy import Nominatim
 
-bot = telebot.TeleBot('7826154830:AAHNPIx0leCLYLvlEgX1xmZIOSSxQyrDxDE')
+geolocator = Nominatim(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                  "(KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+
+bot = telebot.TeleBot('7326578244:AAGWWYsRJ3UwFuyWms7pBLAzrLB2QJjD7yY')
 users = {}
 
 
@@ -19,11 +23,11 @@ def start(message):
         bot.register_next_step_handler(message, get_name)
 
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    user_id = message.from_user.id
-    print(message)
-    bot.send_message(user_id, 'Справочная информация', reply_markup=buttons.main_menu(database.get_all_pr()))
+# @bot.message_handler(commands=['help'])
+# def help(message):
+#     user_id = message.from_user.id
+#     print(message)
+#     bot.send_message(user_id, 'Справочная информация', reply_markup=buttons.main_menu(database.get_all_pr()))
 
 
 def get_name(message):
@@ -31,37 +35,31 @@ def get_name(message):
     user_name = message.text
     bot.send_message(user_id, 'Отлично! Теперь отправьте свой номер!',
                      reply_markup=buttons.num_button())
-    bot.register_next_step_handler(message, get_num, user_name)
+    bot.register_next_step_handler(message, get_num)
 
 
-
-def get_num(message, user_name):
+def get_num(message):
     user_id = message.from_user.id
     if message.contact:
         user_num = message.contact.phone_number
         bot.send_message(user_id, 'Отлично! Теперь отправьте свою локацию!',
                          reply_markup=buttons.loc_button())
-        bot.register_next_step_handler(message, user_name, get_loc, user_num)
+        bot.register_next_step_handler(message, get_loc)
     else:
         bot.send_message(user_id, 'Отправьте контакт по кнопке или отправьте контакт через скрепку!')
-        bot.register_next_step_handler(message, get_num, user_name)
+        bot.register_next_step_handler(message, get_num)
 
 
-def get_loc(message, text, user_name, user_num):
+def get_loc(message):
     user_id = message.from_user.id
     if message.location:
-        user_loc = message.text
-        text += f'Клиент: @{message.from_user.username}'
-        bot.send_message(43331040, text)
-        bot.send_location(43331040, latitude=message.location.latitude, longitude=message.location.longitude)
-        database.register(user_id, user_name, user_num, user_loc)
-        bot.send_message(user_id, 'Регистрация прошла успешно!',
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
-        bot.send_message(user_id, 'Выберите пункт меню:', reply_markup=buttons.main_menu(database.get_pr_buttons()))
-    else:
-        bot.send_message(user_id, 'Отправьте локацию по кнопке или через скрепку!')
-        bot.register_next_step_handler(message, get_loc, text)
+        print(message.location)
+        longitude = message.location.longitude
+        print(longitude)
+        latitude = message.location.latitude
+        print(latitude)
+        address = geolocator.reverse((longitude, latitude)).address
+        print(address)
 
 
-
-bot.polling(non_stop=True)
+bot.infinity_polling()
--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 
-connection = sqlite3.connect('shop', check_same_thread=False)
+connection = sqlite3.connect('shop.db', check_same_thread=False)
 sql = connection.cursor()
 
 
-sql.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER, name TEXT, number TEXT UNIQUE, location NUMERIC);')
+sql.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER, name TEXT, number TEXT UNIQUE, location TEXT);')
 sql.execute('CREATE TABLE IF NOT EXISTS products (pr_id INTEGER PRIMARY KEY AUTOINCREMENT, '
             'pr_name TEXT, pr_des TEXT, pr_price REAL, pr_count INTEGER, pr_photo TEXT);')
 sql.execute('CREATE TABLE IF NOT EXISTS cart(user_id INTEGER, user_product TEXT, pr_amount INTEGER);')
@@ -23,11 +23,11 @@ def check_user(tg_id):
         return False
 
 
-def get_all_pr():
-    return sql.execute('SELECT * FROM products;').fetchall()
-
-
-def get_pr_buttons():
-    all_products = sql.execute('SELECT pr_id, pr_name, pr_count FROM products;').fetchall()
-    in_stock = [n for n in all_products if n[2] > 0]
-    return in_stock
+# def get_all_pr():
+#     return sql.execute('SELECT * FROM products;').fetchall()
+#
+#
+# def get_pr_buttons():
+#     all_products = sql.execute('SELECT pr_id, pr_name, pr_count FROM products;').fetchall()
+#     in_stock = [n for n in all_products if n[2] > 0]
+#     return in_stock
